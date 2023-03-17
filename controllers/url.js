7051a5840637d1aa420c4a26c48031ae7340501c
asimaries <logxtech59@gmail.com>
2023-03-17 23:55:42 +0530

    auth

--- controllers/url.js
+++ controllers/url.js
@@ -6,13 +6,15 @@ async function handleGenrateNewShortURL(req, res) {
 	const body = req.body;
 	if (!body.redirectURL)
 		return res.render('404', { error: 'URL is requried' });
-
+	console.log(req.user)
 	const shortId = shortid();
 	await URL.create({
 		shortId: shortId,
 		redirectURL: body.redirectURL,
 		visitHistory: [],
+		createdBy: req.user._id
 	});
+	// return res.json({ id: shortId });
 	return res.render('home', { id: shortId });
 }
 async function handleAnalytics(req, res) {
@@ -24,7 +26,13 @@ async function handleAnalytics(req, res) {
 }
 
 async function handleAllAnalytics(req, res) {
-	return res.render('analytics', { data: await URL.find() });
+	return res.render('analytics', {
+		data: await URL.find({ createdBy: req.user._id })
+	});
 }
 
-module.exports = { handleGenrateNewShortURL, handleAnalytics, handleAllAnalytics };
+module.exports = {
+	handleGenrateNewShortURL,
+	handleAnalytics,
+	handleAllAnalytics
+};
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser')
 
-const { router } = require('./route/url');
 const { connectToMongoDB } = require('./connect');
 const { URL } = require('./models/url')
+const { restrictToLoggedInUserOnly } = require('./middleware/auth')
+
+const { staticRouter } = require('./route/staticRouter');
+const { router: urlRouter } = require('./route/url');
+const { UserRouter } = require('./route/user');
+
+
 const app = express();
 const PORT = 7777;
 
@@ -15,16 +22,15 @@ app.set('views', path.resolve('./views'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(function (req, res, next) {
-	console.log(new Date(Date.now()).toString())
+	console.log((new Date(Date.now()).toString()).substring(0, 25), req.path)
 	next()
 })
-app.use('/url', router)
 
-app.get('/', (req, res) => {
-	return res.render('home');
-});
-
+app.use('/url', restrictToLoggedInUserOnly, urlRouter)
+app.use('/user', UserRouter)
+app.use('/', staticRouter)
 
 app.get('/url/:shortID', async (req, res) => {
 	const shortID = req.params.shortID;
@@ -38,8 +44,11 @@ app.get('/url/:shortID', async (req, res) => {
 	// console.log(entry);
 	return res.redirect(entry.redirectURL)
 })
-app.get('/url/analytics', async (req, res) => {
-	return res.redirect(entry.redirectURL)
-})
+
+
+// app.get('/url/analytics', async (req, res) => {
+// 	return res.redirect(entry.redirectURL)
+// })
+
 
 app.listen(PORT, console.log(`Server started on http://localhost:${PORT}`));
